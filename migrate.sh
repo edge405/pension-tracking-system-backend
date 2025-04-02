fe5015166ccf261c2a69573eaea652db65a63562
csedge <ejlindayao@gmail.com>
2025-04-02 21:17:27 +0800

    fuck

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # For Unix/Linux-based systems, set the FLASK_APP environment variable
-export FLASK_APP=app.py
+export FLASK_APP=wsgi.py
 
 # Run Flask database migrations and upgrades
 flask db migrate
